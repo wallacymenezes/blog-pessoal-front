5dc6c209630f71fc18e1274fac3f477cd6d1d80f
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-08-02 14:08:56 -0300

    Projeto: Blog Pessoal - V0.1

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//ELEMENTO JXS/TSX => Função que retorna HTML
+
+import Home from "./paginas/Home/Home";
+
+function App() {
+  return(
+    <>
+      <Home></Home>
+    </>
+  )
+}
+
+export default App
---
+++ src/paginas/Home/Home.tsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+    return (
+        <>
+            <h1 className='titulo'>Eu sou a página Home</h1>
+            <h2 className='titulo'>Pagina Principal do Blog Pessoal</h2>
+            <img src="https://i.imgur.com/H88yIo2.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
