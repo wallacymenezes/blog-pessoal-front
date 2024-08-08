b7d739c0ca490d87a192eba99dacfa1c8c76c8dc
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-08-08 09:05:09 -0300

    feat: Incluido o as bibliotecas do axios

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './paginas/Home/Home';
 import Login from './paginas/Login/Login';
-import Cadastro from './paginas/Cadastro/Cadastro';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
@@ -22,7 +21,6 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login/>} />
           <Route path="/home" element={<Home/>} />
-          <Route path="/cadastro" element={<Cadastro/>} />
         </Routes>
 
         <Footer/>
