04b68c1b94586ec231a9ff3eb25912a991ff9aa2
PhysisOfWally <128063050+PhysisOfWally@users.noreply.github.com>
2024-08-08 09:45:56 -0300

    feat: construido a arquitetura do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,32 @@
-//ELEMENTO JXS/TSX => Função que retorna HTML
-
 //import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './paginas/Home/Home';
-import Login from './paginas/Login/Login';
+import './App.css';
+
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login/Login';
+import Cadastro from './paginas/Cadastro/Cadastro';
+import Home from './paginas/Home/Home';
 
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-
-        <Navbar/>
-        
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/home" element={<Home/>} />
-        </Routes>
-
-        <Footer/>
-
-      </BrowserRouter>
+   
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+  
     </>
-);
+  );
 }
 export default App;
